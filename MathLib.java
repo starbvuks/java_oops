69c05655e28c976004091d39ff22b6d636264829
starbvuks <starbvuks@gmail.com>
2022-07-16 16:05:11 +0530

    mathlib

---
+++ MathLib.java
@@ -0,0 +1,14 @@
+public class MathLib {
+    public static void main(String[] args) {
+        double x = 3.14;
+        int y = -9;
+
+        double z = Math.min(x, y);
+        System.out.println(z);
+        double c = Math.abs(y);
+        System.out.println(y);
+        double d = Math.sqrt(x);
+        System.out.println(x);
+    }
+}
+
